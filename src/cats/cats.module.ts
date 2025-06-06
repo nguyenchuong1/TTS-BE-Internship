c19c320721d3dddcb8a122686d218fab2d5c3467
Tan Pham <minhtantanhiep@gmail.com>
2025-06-06 11:17:20 +0700

    fix: restructure folder project

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -5,7 +5,7 @@ import { CatsService } from './cats.service';
 @Module({
   controllers: [CatsController],
   providers: [CatsService],
-  exports: [CatsService]
+  exports: [CatsService],
 })
 export class CatsModule {
   constructor(private catsService: CatsService) {}
