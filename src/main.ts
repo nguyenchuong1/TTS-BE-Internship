6bd19e5ac2d1ce487ef617656d064fcac5aa7a60
nguyenchuong1 <nguyenchuong010866az@gmail.com>
2025-06-09 13:06:47 +0700

    fix: Correct argument for SwaggerModule.setup

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,8 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('cats')
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
   console.log(`Application is running on: http://localhost:3000`);
 }
