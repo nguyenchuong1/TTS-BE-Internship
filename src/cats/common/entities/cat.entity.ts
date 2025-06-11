b54658c61097c916ca4d512b028188bf02d04127
nguyenchuong1 <nguyenchuong010866az@gmail.com>
2025-06-11 10:03:44 +0700

    feat: setup PostgreSQL with TypeORM and define Cat entity

--- src/cats/common/entities/cat.entity.ts
+++ src/cats/common/entities/cat.entity.ts
@@ -1,21 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
-
+import { Column,Entity,PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
 export class Cat {
+  @PrimaryGeneratedColumn()
   @ApiProperty({
     example: 1,
     description: 'The ID of the cat',
   })
   id: number;
+  @Column()
   @ApiProperty({
     example: 'Mèo Cỏ',
     description: 'The name of the cat',
   })
   name: string;
+  @Column()
   @ApiProperty({
     example: 55,
     description: 'The age of the Cat',
   })
   age: number;
+  @Column()
   @ApiProperty({
     example: 'Maine Coon',
     description: 'The breed of the Cat',
